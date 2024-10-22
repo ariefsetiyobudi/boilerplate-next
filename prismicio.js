d024d13cd256f39e03041e4e5a1d6d196f3fae28
ariefsetiyobudi <ariefsetiyobudi@me.com>
2024-10-22 10:22:32 +0700

    Update prismicio.js

--- prismicio.js
+++ prismicio.js
@@ -1,13 +1,9 @@
 require('dotenv').config()
 import * as prismic from '@prismicio/client'
 
-const repositoryName = process.env.PRISMIC_ENDPOINT;
-const accessToken = process.env.PRISMIC_ACCESS_TOKEN; // Set an access token
-const routes = [
-  // Update to match your website's URL structure
-  { type: 'settings', path: '/' }
-];
+const repositoryName = process.env.PRISMIC_ENDPOINT // Prismic Endpoint URL
+const accessToken = process.env.PRISMIC_ACCESS_TOKEN // Set an access token
 
-const client = prismic.createClient(repositoryName, { routes, accessToken });
+const client = prismic.createClient(repositoryName, { accessToken })
 
-export default client;
+export default client
